19d9efdc7625b9b7c84ad55648ff35ad30589ee0
Aulia Rahman <rahman@Aulias-MacBook-Pro.local>
2021-05-06 14:51:06 +0700

    Membuat Halaman Register

---
+++ app/src/main/java/com/codepolitan/belajarngoding/presentation/register/RegisterActivity.kt
@@ -0,0 +1,17 @@
+package com.codepolitan.belajarngoding.presentation.register
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.codepolitan.belajarngoding.R
+import com.codepolitan.belajarngoding.databinding.ActivityRegisterBinding
+
+class RegisterActivity : AppCompatActivity() {
+
+    private lateinit var registerBinding: ActivityRegisterBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        registerBinding = ActivityRegisterBinding.inflate(layoutInflater)
+        setContentView(registerBinding.root)
+    }
+}
