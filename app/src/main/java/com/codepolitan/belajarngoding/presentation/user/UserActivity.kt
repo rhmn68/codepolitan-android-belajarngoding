dd501e4acc349b4c97ab79c7cdcf2e42419b176a
Aulia Rahman <rahman@Aulias-MacBook-Pro.local>
2021-05-25 13:03:29 +0700

    Membuat Halaman User

---
+++ app/src/main/java/com/codepolitan/belajarngoding/presentation/user/UserActivity.kt
@@ -0,0 +1,17 @@
+package com.codepolitan.belajarngoding.presentation.user
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.codepolitan.belajarngoding.R
+import com.codepolitan.belajarngoding.databinding.ActivityUserBinding
+
+class UserActivity : AppCompatActivity() {
+
+    private lateinit var userBinding: ActivityUserBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        userBinding = ActivityUserBinding.inflate(layoutInflater)
+        setContentView(userBinding.root)
+    }
+}
