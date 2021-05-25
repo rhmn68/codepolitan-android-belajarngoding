0142687705ac093af9eb50faae17f676d77dc562
Aulia Rahman <rahman@Aulias-MacBook-Pro.local>
2021-05-25 14:33:02 +0700

    Membuat Custom Dialog dan Menambahkan Utils

---
+++ app/src/main/java/com/codepolitan/belajarngoding/utils/Utils.kt
@@ -0,0 +1,46 @@
+package com.codepolitan.belajarngoding.utils
+
+import android.content.Context
+import android.view.LayoutInflater
+import android.view.View
+import androidx.appcompat.app.AlertDialog
+import com.codepolitan.belajarngoding.databinding.LayoutDialogErrorBinding
+import com.codepolitan.belajarngoding.databinding.LayoutDialogLoadingBinding
+import com.codepolitan.belajarngoding.databinding.LayoutDialogSuccessBinding
+
+fun View.visible(){ visibility = View.VISIBLE }
+fun View.gone(){ visibility = View.GONE }
+fun View.enabled(){ isEnabled = true }
+fun View.disabled(){ isEnabled = false }
+
+fun showDialogLoading(context: Context): AlertDialog{
+    val binding = LayoutDialogLoadingBinding.inflate(LayoutInflater.from(context))
+    return AlertDialog
+        .Builder(context)
+        .setView(binding.root)
+        .setCancelable(false)
+        .create()
+}
+
+fun showDialogSuccess(context: Context, message: String): AlertDialog{
+    val binding = LayoutDialogSuccessBinding.inflate(LayoutInflater.from(context))
+    binding.tvMessage.text = message
+
+    return AlertDialog
+        .Builder(context)
+        .setView(binding.root)
+        .setCancelable(true)
+        .create()
+}
+
+fun showDialogError(context: Context, message: String){
+    val binding = LayoutDialogErrorBinding.inflate(LayoutInflater.from(context))
+    binding.tvMessage.text = message
+
+    AlertDialog
+        .Builder(context)
+        .setView(binding.root)
+        .setCancelable(true)
+        .create()
+        .show()
+}
