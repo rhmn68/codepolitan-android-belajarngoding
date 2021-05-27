2a3cad68b8ee7e024c741a650580d345202a273f
Aulia Rahman <rahman@Aulias-MacBook-Pro.local>
2021-05-27 10:23:22 +0700

    Membuat Pages Adapter

---
+++ app/src/main/java/com/codepolitan/belajarngoding/adapter/PagesAdapter.kt
@@ -0,0 +1,38 @@
+package com.codepolitan.belajarngoding.adapter
+
+import android.content.Context
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.viewpager.widget.PagerAdapter
+import com.codepolitan.belajarngoding.databinding.ItemPageBinding
+import com.codepolitan.belajarngoding.model.Page
+
+class PagesAdapter(private val context: Context): PagerAdapter() {
+
+    var pages = mutableListOf<Page>()
+        set(value) {
+            field = value
+            notifyDataSetChanged()
+        }
+
+    override fun getCount(): Int = pages.size
+
+    override fun isViewFromObject(view: View, `object`: Any): Boolean = view == `object`
+
+    override fun instantiateItem(container: ViewGroup, position: Int): Any {
+        val pageBinding = ItemPageBinding.inflate(LayoutInflater.from(context), container, false)
+
+        bindItem(pageBinding, pages[position])
+        container.addView(pageBinding.root)
+        return pageBinding.root
+    }
+
+    private fun bindItem(pageBinding: ItemPageBinding, page: Page) {
+        pageBinding.rvPages.setHasFixedSize(true)
+//        pageBinding.rvPages.adapter
+    }
+
+    override fun destroyItem(container: ViewGroup, position: Int, `object`: Any) =
+        container.removeView(`object` as View)
+}
