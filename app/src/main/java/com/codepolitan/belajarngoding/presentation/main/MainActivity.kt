95b93fa5b602932dc25ed8487af88d5b1374c371
Aulia Rahman <rahman@Aulias-MacBook-Pro.local>
2021-06-03 16:46:11 +0700

    Menampilkan Data User

--- app/src/main/java/com/codepolitan/belajarngoding/presentation/main/MainActivity.kt
+++ app/src/main/java/com/codepolitan/belajarngoding/presentation/main/MainActivity.kt
@@ -4,16 +4,23 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.os.Handler
 import android.os.Looper
+import android.util.Log
 import android.view.inputmethod.EditorInfo
 import androidx.appcompat.app.AlertDialog
 import androidx.core.widget.addTextChangedListener
+import com.bumptech.glide.Glide
 import com.codepolitan.belajarngoding.R
 import com.codepolitan.belajarngoding.adapter.MaterialsAdapter
 import com.codepolitan.belajarngoding.databinding.ActivityMainBinding
+import com.codepolitan.belajarngoding.model.User
 import com.codepolitan.belajarngoding.presentation.content.ContentActivity
 import com.codepolitan.belajarngoding.presentation.user.UserActivity
 import com.codepolitan.belajarngoding.repository.Repository
+import com.codepolitan.belajarngoding.utils.showDialogError
 import com.codepolitan.belajarngoding.utils.showDialogLoading
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.FirebaseUser
+import com.google.firebase.database.*
 import org.jetbrains.anko.startActivity
 
 class MainActivity : AppCompatActivity() {
@@ -23,6 +30,32 @@ class MainActivity : AppCompatActivity() {
   }
   private lateinit var mainBinding: ActivityMainBinding
   private lateinit var materialsAdapter: MaterialsAdapter
+  private lateinit var userDatabase: DatabaseReference
+  private var currentUser: FirebaseUser? = null
+
+  private var listenerUser = object : ValueEventListener {
+    override fun onDataChange(snapshot: DataSnapshot) {
+      hideLoading()
+      val user = snapshot.getValue(User::class.java)
+      user?.let {
+        mainBinding.apply {
+          tvNameUserMain.text = it.nameUser
+
+          Glide
+            .with(this@MainActivity)
+            .load(it.avatarUser)
+            .placeholder(android.R.color.darker_gray)
+            .into(ivAvatarMain)
+        }
+      }
+    }
+
+    override fun onCancelled(error: DatabaseError) {
+      hideLoading()
+      Log.e("MainActivity", "[onCancelled] - ${error.message}")
+      showDialogError(this@MainActivity, error.message)
+    }
+  }
 
   override fun onCreate(savedInstanceState: Bundle?) {
     super.onCreate(savedInstanceState)
@@ -31,9 +64,19 @@ class MainActivity : AppCompatActivity() {
 
     //Init
     materialsAdapter = MaterialsAdapter()
+    userDatabase = FirebaseDatabase.getInstance().getReference("users")
+    currentUser = FirebaseAuth.getInstance().currentUser
 
+    getDataFirebase()
     getDataMaterial()
     onAction()
+  }
+
+  private fun getDataFirebase() {
+    showLoading()
+    userDatabase
+      .child(currentUser?.uid.toString())
+      .addValueEventListener(listenerUser)
   }
 
   override fun onResume() {
@@ -88,6 +131,7 @@ class MainActivity : AppCompatActivity() {
       }
 
       swipeMain.setOnRefreshListener {
+        getDataFirebase()
         getDataMaterial()
       }
     }
--- app/src/main/java/com/codepolitan/belajarngoding/presentation/user/UserActivity.kt
+++ app/src/main/java/com/codepolitan/belajarngoding/presentation/user/UserActivity.kt
@@ -4,17 +4,46 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.provider.Settings.ACTION_LOCALE_SETTINGS
+import com.bumptech.glide.Glide
 import com.codepolitan.belajarngoding.R
 import com.codepolitan.belajarngoding.databinding.ActivityUserBinding
+import com.codepolitan.belajarngoding.model.User
 import com.codepolitan.belajarngoding.presentation.changepassword.ChangePasswordActivity
 import com.codepolitan.belajarngoding.presentation.login.LoginActivity
+import com.codepolitan.belajarngoding.utils.showDialogError
 import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.FirebaseUser
+import com.google.firebase.database.*
 import org.jetbrains.anko.startActivity
 
 class UserActivity : AppCompatActivity() {
 
     private lateinit var userBinding: ActivityUserBinding
     private lateinit var firebaseAuth: FirebaseAuth
+    private lateinit var userDatabase: DatabaseReference
+    private var currentUser: FirebaseUser? = null
+
+    private var listenerUser = object : ValueEventListener {
+        override fun onDataChange(snapshot: DataSnapshot) {
+            hideLoading()
+            val user = snapshot.getValue(User::class.java)
+            user?.let {
+                userBinding.tvNameUser.text = it.nameUser
+                userBinding.tvEmailUser.text = it.emailUser
+
+                Glide
+                    .with(this@UserActivity)
+                    .load(it.avatarUser)
+                    .placeholder(android.R.color.darker_gray)
+                    .into(userBinding.ivAvatarUser)
+            }
+        }
+
+        override fun onCancelled(error: DatabaseError) {
+            hideLoading()
+            showDialogError(this@UserActivity, error.message)
+        }
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -23,7 +52,10 @@ class UserActivity : AppCompatActivity() {
 
         //Init
         firebaseAuth = FirebaseAuth.getInstance()
+        userDatabase = FirebaseDatabase.getInstance().getReference("users")
+        currentUser = firebaseAuth.currentUser
 
+        getDataFirebase()
         onAction()
     }
 
@@ -44,6 +76,25 @@ class UserActivity : AppCompatActivity() {
                 startActivity<LoginActivity>()
                 finishAffinity()
             }
+
+            swipeUser.setOnRefreshListener {
+                getDataFirebase()
+            }
         }
+    }
+
+    private fun getDataFirebase() {
+        showLoading()
+        userDatabase
+            .child(currentUser?.uid.toString())
+            .addValueEventListener(listenerUser)
+    }
+
+    private fun showLoading(){
+        userBinding.swipeUser.isRefreshing = true
+    }
+
+    private fun hideLoading(){
+        userBinding.swipeUser.isRefreshing = false
     }
 }
